7fd09e320b89c9f26070ed02756ef60e3c60ae8c
marcoFijan <marcofijan@hotmail.com>
2020-05-16 16:24:58 +0200

    more routes added to app.js

--- app.js
+++ app.js
@@ -14,6 +14,21 @@
 const express = require("express");
 const app = express();
 
+var profiles = [
+  {
+    name: 'Bas',
+    age: 22,
+    interests: ['nintendo', 'playstation', 'sword art online'],
+    location: 'Bleiswijk'
+  },
+  {
+    name: 'Danie',
+    age: 20,
+    interests: ['nintendo', 'xbox', 'xenoblade'],
+    location: 'Opmeer'
+  }
+]
+
 // Link the static files
 // app.use("/static", express.static("public"));
 app.use("/static", express.static("static")); //Here you link to the folder static. So when /static is called in html, express will use the folder static. You can name the folder whatever you want as long as you change the express.static(foldername).
@@ -30,17 +45,29 @@ app.get("/register", function(req, res){
 });
 
 // do something at specific routes
-if (req.url === '/about'){
-  res.end("this is my website\n");
-}
-else{
-  res.end('Hello World!\n')
-}
+app.get("/about", function(req, res){
+  if (req.url === '/about'){
+    res.status(200).send('<h1>hi</h1>')
+    res.end("this is my website\n");
+  }
+  else{
+    res.end('Hello World!\n')
+  }
+});
+
+app.get("/mp3", function(req, res){
+  res.sendfile(__dirname + "/static/audio/waaah.mp3");
+  console.log("Why no Wah in smash bros?");
+});
 
 // id Can be generated from everything. For instance, it can be an unique idcode from a database
 app.get("/profile/:id", function(req, res){
   res.send("Dit profiel heeft het id " + req.params.id);
 });
+
+app.use(function(req, res){
+  res.status(404).send('<h1>ERRRRROR</h1>')
+})
 
 app.listen(3000);
 
