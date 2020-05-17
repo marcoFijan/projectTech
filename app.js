c3337dfd56f648d6aca631123b41f636b204214b
marcoFijan <marcofijan@hotmail.com>
2020-05-17 08:53:22 +0200

    played with ejs and added to project with some link and data tweaks. HTML files now obsolete

--- app.js
+++ app.js
@@ -11,8 +11,8 @@
 //   res.end("Hello World!\n")
 // }
 
-const express = require("express");
-const app = express();
+// VARIABLES
+const express = require('express')
 
 var profiles = [
   {
@@ -29,46 +29,60 @@ var profiles = [
   }
 ]
 
-// Link the static files
-// app.use("/static", express.static("public"));
-app.use("/static", express.static("static")); //Here you link to the folder static. So when /static is called in html, express will use the folder static. You can name the folder whatever you want as long as you change the express.static(foldername).
+var testVar = 'ding'
+
+// const app = express();
+express()
+  .use('/static', express.static('static')) //Here you link to the folder static. So when /static is called in html, express will use the folder static. You can name the folder whatever you want as long as you change the express.static(foldername).
+  .set('view engine', 'ejs')
+  .set('views', 'view')
+  .get('/', home)
+  .get('/register', register)
+  .get('/about', about)
+  .get('/mp3', mp3)
+  .get('/profile', profile )
+  .get('/profile/:id', profileID)
+  .listen(3000)
 
 // Make homepage
-app.get("/", function(req, res){
-  // res.send("Dit is de hoofdpagina");
-  res.sendfile(__dirname + "/index.html");
-});
+function home(req, res){
+  res.render('test', {profiles: profiles})
+  // res.sendfile(__dirname + '/index.html')
+}
 
 // Make registerpage
-app.get("/register", function(req, res){
-  res.send("Dit is de registreerpagina");
-});
+function register(req, res){
+  res.send('Dit is de registreerpagina');
+}
 
-// do something at specific routes
-app.get("/about", function(req, res){
+// Make an aboutpage
+function about(req, res){
   if (req.url === '/about'){
     res.status(200).send('<h1>hi</h1>')
-    res.end("this is my website\n");
+    res.end('this is my website\n')
   }
   else{
     res.end('Hello World!\n')
   }
-});
+}
 
-app.get("/mp3", function(req, res){
-  res.sendfile(__dirname + "/static/audio/waaah.mp3");
-  console.log("Why no Wah in smash bros?");
-});
+// Play sound when accessing /mp3
+function mp3(req, res){
+  res.sendfile(__dirname + '/static/audio/waaah.mp3')
+  console.log('Why no Wah in smash bros?')
+}
+
+function profile(req, res){
+  res.render('profile')
+}
 
 // id Can be generated from everything. For instance, it can be an unique idcode from a database
-app.get("/profile/:id", function(req, res){
-  res.send("Dit profiel heeft het id " + req.params.id);
-});
+function profileID(req, res){
+  res.send('Dit profiel heeft het id ' + req.params.id)
+}
 
-app.use(function(req, res){
-  res.status(404).send('<h1>ERRRRROR</h1>')
-})
+// app.use(function(req, res){
+//   res.status(404).send('<h1>ERRRRROR</h1>')
+// })
 
-app.listen(3000);
-
-console.log("localhost:3000");
+console.log('localhost:3000');
